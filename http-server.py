e3effbc35e701b407204b19554a11b200d3bf505
Emirhan Yücel <83589903+emirhanyucell@users.noreply.github.com>
2023-01-15 19:39:15 +0300

    Add files via upload

---
+++ http-server.py
@@ -0,0 +1,11 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'Hello, World!')
+
+httpd = HTTPServer(('localhost', 8000), MyHandler)
+httpd.serve_forever()
